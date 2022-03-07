7ebe5b03ebeb9629088c328550cca541aad4f38c
Nasir <nasir-islam@hotmail.com>
2022-03-07 16:42:43 +0000

    add post submission functionality from bnta assessment day

--- app.js
+++ app.js
@@ -33,3 +33,39 @@ handleButtonClick = function () {
 }
 
 darkModeButton.addEventListener("click", handleButtonClick)
+
+
+
+
+//Creating new posts with form
+const form = document.getElementById('newPost');
+form.addEventListener('submit', (event) => {
+
+    // stop form submission
+    event.preventDefault();
+
+    const container = document.querySelector('#post-container');
+
+    const post = document.createElement('section');
+    post.classList.add('post');
+    
+    const postTitle = document.createElement('h3');
+    postTitle.innerText = form.elements['postTitle'].value
+    post.appendChild(postTitle)
+    
+    const postDate = document.createElement('h4');
+    postDate.innerText = `Date: ${form.elements['postDate'].value}`
+    post.appendChild(postDate)
+
+    const postText = document.createElement('p');
+    postText.innerText = form.elements['postText'].value
+    post.appendChild(postText)
+
+    container.appendChild(post);
+
+    form.elements['postTitle'].value = ""
+    form.elements['postDate'].value = ""
+    form.elements['postText'].value = ""
+
+    
+});

e1262592d3b9c76ebc06d1b00fc15d4c37bbe82a
Nasir <nasir-islam@hotmail.com>
2022-03-07 16:33:30 +0000

    complete dark mode function

--- app.js
+++ app.js
@@ -9,47 +9,27 @@ handleButtonClick = function () {
         
     }else{
         console.log("Setting to light mode")
+        darkModeButton.innerText = "Dark Mode"
     }
-    
-    console.log("Button Clicked")
-    // change the background colour and text
-    // body = document.body
 
-    // body.style.backgroundColor = "#091d1e"
-    // body.style.color = "#aaaaaa"
-    // body.classList.toggle
     body = document.body
     body.classList.toggle("darkMode")
 
     // change the border of the title
-
     title = document.getElementById("title-section")
-    // title.style.border = "1px solid #aaaaaa"
     title.classList.toggle("darkMode");
 
 
     // change the border and headings on the posts
+    posts = document.getElementsByClassName("post");
+    for (post of posts) {
+        post.classList.toggle("darkMode");
+    }
 
-    // posts = document.getElementsByClassName("post")
-    // for (post of posts){
-        // post.style.border = "1px solid #aaaaaa"
-    // }
-
-    // postHeadings = document.getElementsByTagName("h3")
-    // for (heading of postHeadings){
-        // heading.style.color = "#466876"
-    // }
-
-        posts = document.getElementsByClassName("post")
-        for (post of posts){
-            post.classList.toggle("darkMode");
-        }
-
-
-        postHeadings = document.getElementsByTagName("h3")
-        for (heading of postHeadings){
-            heading.classList.toggle("darkMode");
-        }
+    postHeadings = document.getElementsByTagName("h3")
+    for (heading of postHeadings){
+        heading.classList.toggle("darkMode");
+    }
 }
 
 darkModeButton.addEventListener("click", handleButtonClick)
